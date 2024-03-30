e76869796788e9d1f72188e3f2ed4336b4b44108
BizBrainiac <125855598+BizBrainiac@users.noreply.github.com>
2024-03-30 18:12:19 +0530

    Update mk.h

--- mk.h
+++ mk.h
@@ -1,7 +1,7 @@
 #ifndef MK_H
 #define MK_H
 
-void inputStudentData(char name[], float exam1_marks[], float exam2_marks[]);
+void inputStudentData(char name[], float ISA1[], float ISA2[]);
 float calAvg(float marks[], int size);
 char gradeAssignment(float avg);
 void genGradeCard(char name[], char grade1, char grade2);
